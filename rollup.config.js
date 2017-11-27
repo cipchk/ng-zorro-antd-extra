dabafdea8a9b1d214dcb4d43e33d281625111bdb
cipchk <cipchk@qq.com>
2017-11-27 19:32:08 +0800

    build: produce angular package format compatible code

---
+++ rollup.config.js
@@ -0,0 +1,32 @@
+const resolve = require('rollup-plugin-node-resolve');
+const replace = require('rollup-plugin-replace');
+
+const globals = {
+    '@angular/core': 'ng.core',
+    '@angular/common': 'ng.common',
+    '@angular/platform-browser': 'ng.platformBrowser',
+    '@angular/router': 'ng.router',
+    '@angular/common/http': 'ng.common.http',
+
+    'ng-zorro-antd': 'ngZorro.antd',
+
+    'rxjs/observable/fromEvent': 'Rx.Observable'
+};
+
+module.exports = {
+    sourcemap: true,
+    rollup: require('rollup'),
+    context: 'this',
+    name: 'ngZorro.antd_extra',
+    output: 'ng-zorro-antd-extra.umd.js',
+    format: 'umd',
+    plugins: [
+        replace({ "import * as moment": "import moment" }),
+        resolve({
+            jsnext: true,
+            main: true
+        })
+    ],
+    external: Object.keys(globals),
+    globals: globals
+};
